346a3ecfebddc5f66327735402c3e79d6c21d67b
Gabriel Vieira Teodoro <gabrielvt14@hotmail.com>
2017-05-17 01:07:48 -0300

    Atualização da atividade 12 - Cadastrar.php

--- Atividade_12/core/Cadastrar.php
+++ Atividade_12/core/Cadastrar.php
@@ -6,6 +6,9 @@ require_once 'Aluno.php';
 class Cadastrar extends Aluno
 {
 	protected $aluno;
+	public $dns = 'mysql:host=localhost;dbname';
+	public $user = 'root';
+	public $pass = '';
 
 	public function __construct($aluno)
 	{
@@ -15,21 +18,27 @@ class Cadastrar extends Aluno
 
 	public function insert()
 	{
-		$pdo = new Banco();
-		$insert = "INSERT INTO aluno(nome, nota1, nota2, nota3, nota4)
-				VALUES(:nome, :nota1, :nota2, :nota3, :nota4)";
-		$stmt = $pdo->prepare($insert);
-		$stmt->bindValue(':nome', $this->aluno['nomeAluno']);
-		$stmt->bindValue(':nota1', $this->aluno['nota1']);
-		$stmt->bindValue(':nota2', $this->aluno['nota2']);
-		$stmt->bindValue(':nota3', $this->aluno['nota3']);
-		$stmt->bindValue(':nota4', $this->aluno['nota4']);
-
-		if ($stmt->execute()) {
-			header('Location: ../index.php');
-		} else {
-			die('Erro ao cadastrar aluno');
+		try {
+			$pdo = new PDO($this->dns, $this->user, $this->pass);
+		} catch (PDOException $e) {
+			die('ERROOOOO' . $e->getMessage());
 		}
+		$insert = "INSERT INTO aluno SET nome = '$this->aluno->getNome()', nota1 = '$this->aluno->getN1()', nota2 = '$this->getN2()', nota3 = '$this->getN3()', nota4 = '$this->getN4()'";
+	/*	$stmt = $pdo->prepare($insert);
+		$stmt->bindValue(':nome', $this->aluno->getNome());
+		$stmt->bindValue(':nota1', $this->aluno->getN1());
+		$stmt->bindValue(':nota2', $this->aluno->getN2());
+		$stmt->bindValue(':nota3', $this->aluno->getN3());
+		$stmt->bindValue(':nota4', $this->aluno->getN4());*/
+
+		$pdo->query($insert);
+
+	//	if ($stmt->execute()) {
+	//		header('Location: ../index.php');	
+	//	} else {
+	//		die('Erro ao cadastrar aluno');
+			//var_dump($this->aluno->getNome());exit;
+	//	}
 	}
 }
 
