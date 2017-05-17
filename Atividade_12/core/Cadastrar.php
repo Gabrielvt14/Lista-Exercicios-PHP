4baf8d381c7dc866238954f42e316e0b1ddd9212
Gabrielvt14 <gabrielvt14@hotmail.com>
2017-05-17 10:41:30 -0300

    atualização atividade 12

--- Atividade_12/core/Cadastrar.php
+++ Atividade_12/core/Cadastrar.php
@@ -8,7 +8,7 @@ class Cadastrar extends Aluno
 	protected $aluno;
 	public $dns = 'mysql:host=localhost;dbname';
 	public $user = 'root';
-	public $pass = '';
+	public $pass = 'vertrigo';
 
 	public function __construct($aluno)
 	{
@@ -26,13 +26,13 @@ class Cadastrar extends Aluno
 		$insert = "INSERT INTO aluno(nome, nota1, nota2, nota3, nota4)
 					VALUES(:nome, :nota1, :nota2, :nota3, :nota4)";
 		$stmt = $pdo->prepare($insert);
-		$stmt->bindValue(':nome', $this->aluno->getNome());
-		$stmt->bindValue(':nota1', $this->aluno->getN1());
-		$stmt->bindValue(':nota2', $this->aluno->getN2());
-		$stmt->bindValue(':nota3', $this->aluno->getN3());
-		$stmt->bindValue(':nota4', $this->aluno->getN4());
+		$stmt->bindValue(':nome', $this->aluno->getNome());	//	result: true
+		$stmt->bindValue(':nota1', $this->aluno->getN1());	//	result: true
+		$stmt->bindValue(':nota2', $this->aluno->getN2());	//	result: true
+		$stmt->bindValue(':nota3', $this->aluno->getN3());	//	result: true
+		$stmt->bindValue(':nota4', $this->aluno->getN4());	//	result: true
 
-		if ($stmt->execute()) {
+		if ($stmt->execute()) {	//	da maneira que esta o código execute() retorna false
 			header('Location: ../index.php');	
 		} else {
 			die('Erro ao cadastrar aluno');
